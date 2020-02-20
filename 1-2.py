4b822833ccec554fc7321260a6652517ba192176
zhangmh <zhangmh@sdysit.com>
2020-02-20 10:16:20 +0800

    添加第一章相应的代码

---
+++ 1-2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import wx
+
+class MainFrame(wx.Frame):
+    def __init__(self, parent):
+
+        wx.Frame.__init__(self, parent, -1, "Hello World", 
+            size=(800, 600), style=wx.DEFAULT_FRAME_STYLE)
+
+class MainApp(wx.App):
+
+    def OnInit(self):
+
+        self.SetAppName("Hello World")
+        self.frame = MainFrame(None)
+        self.frame.Show()
+
+        return True
+
+def main():
+    app = MainApp()
+    app.MainLoop()
+   
+if __name__ == '__main__':
+    main()
+
+
+    
